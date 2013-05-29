93d29bd3e5e0745ef61de072e97fbcb52fdfe99b
Pat <nemski.rabbit@gmail.com>
2013-05-29 21:50:14 +1000

    cleaned up compile shell script. I will create a make file... oneday

--- compile.sh
+++ compile.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-# Becaude I don't know how to use a makefile
-gcc -m64 -D_KERNEL  -mcmodel=kernel -DSVR4 -O2 -c vixen.c
-gcc -D_KERNEL -DSVR4 -O2 -c vixen.c
+# Because I don't know how to use a makefile
+gcc -m64 -D_KERNEL -mcmodel=kernel -DSVR4 -O2 -c vixen.c
+gcc -o fox fox.c
 
+mkdir /root/vixen
+mv vixen /root/vixen/
+mv fox /root/vixen/
